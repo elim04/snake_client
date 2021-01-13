c669272d583344a8b1815f58a7b951645578fece
Emily Lim <emily.elim04@gmail.com>
2021-01-13 18:44:03 +0000

    creating handleUserInput function to allow you to quit the game using ctrlc

--- play.js
+++ play.js
@@ -3,3 +3,23 @@
 const { connect } = require('./client');
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  //to handle the user input and allow you to quit the game using ctrl c, when you pass those keys to it, it will quit
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+     return process.exit();
+    };
+  }
+
+  //the function that when you input ctrl c, it will do this call back function
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+setupInput();
