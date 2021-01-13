5af72cf504610774d080a4d32dbcfe0f42916c8a
Emily Lim <emily.elim04@gmail.com>
2021-01-13 18:18:12 +0000

    learning how to use the move command and experimenting with it

--- client.js
+++ client.js
@@ -11,13 +11,31 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  //establishing event data handler
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
+  //once you have connected, it will console log the statement to you and write your name to the snake in the server
   conn.on('connect', () => {
     console.log("You have successfully connected to the game server!");
     conn.write("Name: EEL");
+  })
+
+  conn.on('connect', () => {
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 100);
+
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 200);
+
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 100)
+
   })
 
   return conn;
